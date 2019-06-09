a971f4ee5592eef4a8f5220a3a51987dd8e71280
patrick-salvatore <salvatorepat8@gmail.com>
2019-06-09 19:07:09 -0400

    banner styling

--- client/src/components/Home/Content/ServicesList/ServicesList.js
+++ client/src/components/Home/Content/ServicesList/ServicesList.js
@@ -5,28 +5,26 @@ import './ServicesList.scss'
 
 const ServicesList = () => {
     return (
-        <div className = 'image-container'> 
-            <div className = 'image-list'>
-                <div className = 'image'>
-                    <img src= {require('../../../../assets/cover/TrainWithUs.jpg')} alt= "Traing With Us" /> 
-                    <div className = 'button-actions'>
-                        <button>Group Training</button>
-                    </div>
-                </div>
-                <div className = 'image'>
-                    <img src= {require('../../../../assets/cover/Servicing.jpg')} alt= "Servicing"/> 
-                    <div className = 'button-actions'>
-                        <button>Bike Servicing</button>
-                    </div>
-                </div>
-                <div className = 'image'>
-                    <img src= {require('../../../../assets/cover/Blog.jpg')} alt= "Blog"/> 
-                    <div className = 'button-actions'>
-                        <button>Blog</button>
-                    </div>
-                </div>
+    <div className = 'image-list'>
+        <div className = 'image'>
+            <img src= {require('../../../../assets/cover/TrainWithUs.jpg')} alt= "Traing With Us" /> 
+            <div className = 'button-actions'>
+                <button>Group Training</button>
             </div>
         </div>
+        <div className = 'image'>
+            <img src= {require('../../../../assets/cover/Servicing.jpg')} alt= "Servicing"/> 
+            <div className = 'button-actions'>
+                <button>Bike Servicing</button>
+             </div>
+        </div>
+        <div className = 'image'>
+            <img src= {require('../../../../assets/cover/Blog.jpg')} alt= "Blog"/> 
+            <div className = 'button-actions'>
+                <button>Blog</button>
+            </div>
+        </div>
+    </div>
     )
 }
 
